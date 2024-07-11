dfb9583ec6508b6f3d3bbc47c0aaa8a05141555e
NoketeX <rubens.castro@student.ateneo.edu>
2024-07-11 15:00:51 +0800

    Minor Edits

--- auth.js
+++ auth.js
@@ -1,16 +1,36 @@
-import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+import { 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    signOut,
+    onAuthStateChanged,
+    inMemoryPersistence,
+    browserSessionPersistence,
+    setPersistence,
+} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+
 import { } from "/firestore_UNIV_v2_mirror.js"; 
 
 const AUTH = getAuth();
 
+export function getCurrentUser(){
+    return AUTH.currentUser;
+}
 // Sign in function
 export function signIn(email, password) {
     signInWithEmailAndPassword(AUTH, email, password)
         .then((userCredential) => {
             // Signed in successfully
             const USER = userCredential.user;
-            console.log("User signed in:", USER);
-            window.location.replace("index.html");
+                
+            onAuthStateChanged(AUTH, (user) => {
+                if(user) {
+                    console.log("User signed in:", USER);
+                    window.location.replace("index.html");
+                }
+                else {
+                    console.log("User not signed in");
+                }
+            })
         })
         .catch((error) => {
             // Handle errors
