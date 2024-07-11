a7e02a6704e1118824015707eca5dcac040d7812
NoketeX <rubens.castro@student.ateneo.edu>
2024-07-11 13:06:50 +0800

    Preliminary Authentication Setup

--- auth.js
+++ auth.js
@@ -1,16 +1,16 @@
 import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
 import { } from "/firestore_UNIV_v2_mirror.js"; 
 
-const auth = getAuth();
+const AUTH = getAuth();
 
 // Sign in function
 export function signIn(email, password) {
-    signInWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(AUTH, email, password)
         .then((userCredential) => {
             // Signed in successfully
-            console.log(userCredential); 
-            const user = userCredential.user;
-            console.log("User signed in:", user);
+            const USER = userCredential.user;
+            console.log("User signed in:", USER);
+            window.location.replace("index.html");
         })
         .catch((error) => {
             // Handle errors
@@ -20,13 +20,14 @@ export function signIn(email, password) {
 
 // Sign out function
 export function signOutUser() {
-    signOut(auth)
+    signOut(AUTH)
         .then(() => {
             // Signed out successfully
             console.log("User signed out");
+            window.location.replace("login.html");
         })
         .catch((error) => {
             // Handle errors
             console.error("Error signing out:", error);
         });
-}
+}   
