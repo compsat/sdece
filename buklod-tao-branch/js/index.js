c3ec5a443d359bfceeec840da860de1dc2c80c5a
Marvin-Jae-Estrella <marvin.estrella@student.ateneo.edu>
2024-07-19 13:30:30 +0800

    Manual merging of dxo-59 with latest dxo-119

--- buklod-tao-branch/js/index.js
+++ buklod-tao-branch/js/index.js
@@ -31,6 +31,28 @@ var popup = L.popup();
 
 // function to store the html for info display on pin click
 function onPinClick(doc) {
+  // Variables for risk levels
+	var earthquake = doc.earthquake_risk;
+	var earthquake_split = earthquake.split(' RISK: ');
+	var earthquake1 = earthquake_split[0];
+	var earthquake2 = earthquake_split[1];
+	var fire = doc.fire_risk;
+	var fire_split = fire.split(' RISK: ');
+	var fire1 = fire_split[0];
+	var fire2 = fire_split[1];
+	var flood = doc.flood_risk;
+	var flood_split = flood.split(' RISK: ');
+	var flood1 = flood_split[0];
+	var flood2 = flood_split[1];
+	var landslide = doc.landslide_risk;
+	var landslide_split = landslide.split(' RISK: ');
+	var landslide1 = landslide_split[0];
+	var landslide2 = landslide_split[1];
+	var storm = doc.storm_risk;
+	var storm_split = storm.split(' RISK: ');
+	var storm1 = storm_split[0];
+	var storm2 = storm_split[1];
+
 	let leaflet_html = `
   <div class="leafletPopupContainer" id="leafletModal">
     <div class="leafletHeader">
@@ -38,15 +60,15 @@ function onPinClick(doc) {
     </div>
     <div class="leafletContent">
       <br>
-      <div styel="line-height: 1px;">
+      <div style="line-height: 1px;">
         <p>${doc.contact_number}</p>
         <p>${doc.household_address}</p>
       </div>
-      <div class="modal-line">
+      <div class="modalLine" style="width: 100%">
         <label class="leafletLabel">Residency Status</label>
         <label class="leafletLabel">Part of HOA/NOA</label>
       </div>
-      <div class="modal-line" style="line-height: 3px; margin-bottom: 2px;">
+      <div class="modalLine" style="line-height: 3px; margin-bottom: 2px; width: 180px;">
         <p class="leafletDetails">${doc.residency_status}</p>
         <p class="leafletDetails">${doc.is_hoa_noa}</p>
       </div>
@@ -58,72 +80,72 @@ function onPinClick(doc) {
       <div class="leafletSubHeader">
         <label>Risk Levels</label>
       </div>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Earthquake</label>
-        <label class="leafletLabel">RISK LEVEL</label>
+        <label class="leafletLabel">${earthquake1}</label>
       </div>
       <div>
-        <p class="leafletDetails">${doc.earthquake_risk}</p>
+        <p class="leafletDetails">${earthquake2}</p>
       </div>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Fire</label>
-        <label class="leafletLabel">RISK LEVEL</label>
+        <label class="leafletLabel">${fire1}</label>
       </div>
       <div>
-        <p class="leafletDetails">${doc.fire_risk}</p>
+        <p class="leafletDetails">${fire2}</p>
       </div>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Flood</label>
-        <label class="leafletLabel">RISK LEVEL</label>
+        <label class="leafletLabel">${flood1}</label>
       </div>
       <div>
-        <p class="leafletDetails">${doc.flood_risk}</p>
+        <p class="leafletDetails">${flood2}</p>
       </div>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Landslide</label>
-        <label class="leafletLabel">RISK LEVEL</label>
+        <label class="leafletLabel">${landslide1}</label>
       </div>
       <div>
-        <p class="leafletDetails">${doc.landslide_risk}</p>
+        <p class="leafletDetails">${landslide2}</p>
       </div>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Storm</label>
-        <label class="leafletLabel">RISK LEVEL</label>
+        <label class="leafletLabel">${storm1}</label>
       </div>
       <div>
-        <p class="leafletDetails">${doc.storm_risk}</p>
+        <p class="leafletDetails">${storm2}</p>
       </div>
     </div>
     <div>
       <div class="leafletSubHeader">
         <label>Residents</label>
       </div>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Total</label>
         <label class="leafletLabel">${doc.number_residents}</label>
       </div>
       <hr style="border-top: 1px solid #CBD5E0;">
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Minors</label>
         <label class="leafletLabel">${doc.number_minors}</label>
       </div>
       <br>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Seniors</label>
         <label class="leafletLabel">${doc.number_seniors}</label>
       </div>
       <br>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">PWD</label>
         <label class="leafletLabel">${doc.number_pwd}</label>
       </div>
       <br>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Sick</label>
         <label class="leafletLabel">${doc.number_sick}</label>
       </div>
       <br>
-      <div class="modal-line">
+      <div class="modalLine">
         <label class="leafletLabel">Pregnant</label>
         <label class="leafletLabel">${doc.number_pregnant}</label>
       </div>
@@ -172,7 +194,7 @@ function onMapClick(e) {
             ${lat} + ${lng}
             <br>
       </div>
-    <button class="addButton p-5" data-lat="${lat}" data-lng="${lng}">Add Location</button>
+    <button id="mainButton" class="addButton p-5" data-lat="${lat}" data-lng="${lng}">Add Household</button>
     `;
 
 	popup.setLatLng(e.latlng).setContent(popupContent).openOn(map);
@@ -197,6 +219,44 @@ function onMapClick(e) {
 		modal.classList.remove('hidden');
 		modal.classList.add('flex');
 
+ // Handle form submission
+ document.getElementById('addHouseholdForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  
+  const householdData = {
+    household_name: document.getElementById('household_name').value,
+    contact_number: document.getElementById('contact_number').value,
+    household_address: document.getElementById('household_address').value,
+    residency_status: document.getElementById('residency_status').value,
+    is_hoa_noa: document.getElementById('is_hoa_noa').value,
+    nearest_evac: document.getElementById('nearest_evac').value,
+    earthquake_risk: document.getElementById('earthquake_risk').value,
+    fire_risk: document.getElementById('fire_risk').value,
+    flood_risk: document.getElementById('flood_risk').value,
+    landslide_risk: document.getElementById('landslide_risk').value,
+    storm_risk: document.getElementById('storm_risk').value,
+    number_residents: document.getElementById('number_residents').value,
+    number_minors: document.getElementById('number_minors').value,
+    number_seniors: document.getElementById('number_seniors').value,
+    number_pwd: document.getElementById('number_pwd').value,
+    number_sick: document.getElementById('number_sick').value,
+    number_pregnant: document.getElementById('number_pregnant').value,
+    location_coordinates: new firebase.firestore.GeoPoint(parseFloat(lat), parseFloat(lng))
+  };
+
+  addDoc(colRef, householdData)
+    .then(() => {
+      alert('Household added successfully!');
+      modal.classList.add('hidden');
+      modal.classList.remove('flex');
+      location.reload(); // Reload the map to show the new marker
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
+});
+
+
 		// Close the modal when the user clicks anywhere outside of it
 		window.onclick = function (event) {
 			if (event.target == modal) {
@@ -206,6 +266,8 @@ function onMapClick(e) {
 		};
 	});
 }
+
+map.on('click', onMapClick);
 
 //// Event Listeners
 searchControl.on('results', function (data) {
@@ -221,26 +283,26 @@ searchControl.on('results', function (data) {
 //script for add household modal
 
 // modal
-var formModal = document.getElementById('formModal');
+var formModal = document.getElementById('addModal');
 
 // open modal
-var openForm = document.getElementById('addHousehold');
+var openForm = document.getElementById('mainButton');
 
 // Get the <span> element that closes the modal
 var closeForm = document.getElementsByClassName('closeForm')[0];
 
 // When the user clicks the button, open the modal
-// openForm.onclick = function() {
-//   formModal.style.display = "block";
-// }
+openForm.onclick = function() {
+   formModal.style.display = "block";
+}
 
-// openForm.addEventListener('click', function () {
-// 	formModal.style.display = 'block';
-// });
+openForm.addEventListener('click', function () {
+	formModal.style.display = 'block';
+});
 
-// closeForm.addEventListener('click', function () {
-// 	formModal.style.display = 'none';
-// });
+closeForm.addEventListener('click', function () {
+  formModal.style.display = 'none';
+});
 
 // Closing the modal if the user clicks outside of it
 window.onclick = function (event) {
