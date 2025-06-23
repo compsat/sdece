7143786282f722e48282a0227330e38a12905677
RG55 <johannesesmele@gmail.com>
2025-06-23 13:22:43 +0800

    Revert to test

--- buklod-tao-branch/js/index.js
+++ buklod-tao-branch/js/index.js
@@ -16,7 +16,7 @@ import {
 } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js';
 import evacCenters from '/hardcode/evac-centers.json' with {type: 'json'};
 
-setCollection("buklod-official");
+setCollection("buklod-official-TEST");
 
 var colRef = getCollection();
 var partnersArray = getPartnersArray();
