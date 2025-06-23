90d577756f0aa075cbc8167b380162469734153a
Halleyscomet0855 <hunterxhalley@gmail.com>
2025-06-23 12:25:47 +0800

    renamed folders to be more compliant with the specs

---
+++ app_sdece/js/index.js
@@ -0,0 +1,23 @@
+export function showMainModal() {
+	var mainModal = document.getElementById('mainModal');
+	mainModal.style.display = 'flex';
+
+	// window.onclick = function (event) {
+	// 	if (event.target == mainModal) {
+	// 		mainModal.style.display = 'none';
+	// 	}
+	// };
+}
+
+export function showAddModal() {
+	var addModal = document.getElementById('addModal');
+	addModal.style.display = 'flex';
+
+	// window.onclick = function (event) {
+	// 	if (event.target == addModal) {
+	// 		addModal.style.display = 'none';
+	// 	}
+	// };
+}
+
+
