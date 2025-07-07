1125d4770dbef28afeebc28da0d619368fe657f9
franzcarmen01 <franzene.carmen@student.ateneo.edu>
2025-07-07 14:09:03 +0800

    Fix: Line errors

--- app_sdece/js/firestore.js
+++ app_sdece/js/firestore.js
@@ -303,8 +303,10 @@ getDocs(col_ref)
 			containerDiv.appendChild(img);
 			containerDiv.appendChild(listItem);
 			locationList.appendChild(containerDiv);
-	}});
+
+		});
 	})
+
 	.catch((error) => {
 		console.error('Error getting documents: ', error);
 	});
