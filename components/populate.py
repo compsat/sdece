668c09acc648efe03cd07cd0305c97f4ea51b855
Halleyscomet0855 <hunterxhalley@gmail.com>
2025-07-15 15:07:34 +0800

    edited population script to account for datetime

--- components/populate.py
+++ components/populate.py
@@ -1,11 +1,14 @@
 import openpyxl
 import json
+import datetime
 
-filename = "CRA.xlsx"
+filename = "./CRA.xlsx"
 entries = []
+
+# This file takes a file called CRA.xlsx, with the specific worksheet called "for_encoding"
 def read_from_file(filename, row_index):
     wb = openpyxl.load_workbook(filename)
-    ws = wb["for_encoding"]
+    ws = wb["for_encoding"] 
 
     for row in range(2, row_index+1):
         entry = {}
@@ -16,9 +19,17 @@ def read_from_file(filename, row_index):
 
     #print(entries)
 
+def date_serializer(obj):
+
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    else:
+        return obj
+
+
 def export_file(filename):
     with open(filename, 'w') as fp:
-        json.dump(entries, fp, indent=4)
+        json.dump(entries, fp, indent=4, default=date_serializer)
 
 read_from_file(filename,30)
 export_file("output.json")
