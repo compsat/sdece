b366714ae8cb213359d46aa8339201297e0c9f2a
ayenlatorres <iyanla.torres@student.ateneo.edu>
2025-07-18 10:57:09 +0800

    More proper location_link regex

--- js/firestore_UNIV.js
+++ js/firestore_UNIV.js
@@ -224,7 +224,7 @@ const VALIDATION_RULES = {
 			enum: ['HOA', 'NOA', 'N/A'],
 		},
     location_coordinates: {label: "Location Coordinates", type: 'object', required: true },
-		location_link: { label: "Location Link", type: 'string', required: true, regex: /^https:+/ }, // data validation for link
+		location_link: { label: "Location Link", type: 'string', required: true, regex: /^https:\/\/.+\..+/ }, // data validation for link
 		household_address: { label: "Household Address", type: 'string', required: true, maxLength: 100 },
 		household_material: {
       label: "Household Material",
@@ -285,7 +285,7 @@ const VALIDATION_RULES = {
 			enum: ['HOA', 'NOA', 'N/A'],
 		},
     location_coordinates: { label: "Location Coordinates", type: 'object', required: true },
-		location_link: { label: "Location Link", type: 'string', required: true, regex: /^https:\/\/www\.openstreetmap\.org\/.+/  },
+		location_link: { label: "Location Link", type: 'string', required: true, regex: /^https:\/\/.+\..+/  },
 		household_address: { label: "Household Address", type: 'string', required: true, maxLength: 100 },
 		household_material: {
       label: "Household Material",
