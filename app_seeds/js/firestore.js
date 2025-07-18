2467348f57e05fe41c99a08937bc5b898cad561c
Jade <jade.ramos@student.ateneo.edu>
2025-07-18 13:19:59 +0800

    Added alert when pressing the back button

--- app_seeds/js/firestore.js
+++ app_seeds/js/firestore.js
@@ -432,6 +432,7 @@ function showEditActivityForm(activity, partnerName, coords) {
 	backBtn.className = 'modal-back-btn';
 	backBtn.innerHTML = `<svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.5 19L8.5 12L15.5 5" stroke="#222b45" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
 	backBtn.onclick = function() {
+		alert("Any changes would not be saved. Proceed?");
 		showActivityDetailModal(activity, partnerName, coords);
 	};
 	headerRow.appendChild(backBtn);
