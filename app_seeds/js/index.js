2acbc313a7a5d64a7c69570a37e12f3525edaece
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2026-06-24 14:06:14 +0800

    Add export feature and button

--- app_seeds/js/index.js
+++ app_seeds/js/index.js
@@ -1,3 +1,6 @@
+import { SEEDS_RULES } from "/js/firestore_UNIV.js";
+import * as XLSX from "https://cdn.sheetjs.com/xlsx-0.20.3/package/xlsx.mjs";
+
 export function showMainModal() {
 	var mainModal = document.getElementById('mainModal');
 	mainModal.style.display = 'flex';
@@ -7,3 +10,41 @@ export function showAddModal() {
 	var addModal = document.getElementById('addModal');
 	addModal.style.display = 'flex';
 }
+
+/**
+ * Exports current data into an Excel (.xlsx) file, with activity per row. Grouped and sorted by partner name.
+ * 
+ * @example 
+ * document.getElementById('download-report').addEventListener("click", exportData);
+ * 
+ * @global
+ * @requires XLSX - SheetJS Library
+ * @requires SEEDS_RULES - Rule Engine for SEEDS documents
+ * @requires window.partners - JS Object mapping partner names to an array of activities
+ */
+export function exportData() {
+	let partners = window.partners;
+
+    const workbook = XLSX.utils.book_new();
+	const sheetData = [["Partner", ...SEEDS_RULES[2]]]
+	for (const [partnerName, activities] of Object.entries(partners).sort((a, b) => a[0].localeCompare(b[0]))) {
+		activities.forEach(activity => {
+			sheetData.push([
+				partnerName,
+				...SEEDS_RULES[2].map(field => {
+					let val;
+					if (field === "partner_coordinates" 
+						&& activity[field])
+						val = `${activity[field]._lat}, ${activity[field]._long}`;
+					else val = activity[field] ?? "";
+					return val;
+				})
+			])
+		})
+	}
+	XLSX.utils.book_append_sheet(workbook, XLSX.utils.aoa_to_sheet(sheetData), 'Master Sheet');
+	const now = new Date()
+    XLSX.writeFile(workbook, `Seeds_Report-${now.toLocaleDateString().replaceAll('/','-')}.xlsx`);
+}
+
+document.getElementById('download-report').addEventListener("click", exportData);
