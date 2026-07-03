8a0179b9fd9586b9a0581005b9c94f73d052464a
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2026-07-03 18:50:02 +0800

    Add removeDatabase function

--- app_seeds/js/dexie.js
+++ app_seeds/js/dexie.js
@@ -1,6 +1,11 @@
+import { addRxPlugin } from 'https://esm.sh/rxdb@15.18.0';
+import { RxDBLeaderElectionPlugin } from 'https://esm.sh/rxdb@15.18.0/plugins/leader-election';
+
 import { addCollection, createDatabase } from '../../js/dexie_UNIV.js'
 import { SEEDS_RULES_TEST, SEEDS_RULES } from '../../js/firestore_UNIV.js';
 import { startFirestoreSync } from './firestore.js';
+
+addRxPlugin(RxDBLeaderElectionPlugin);
 
 let db = null;
 let activeSeedsCollection = null;
@@ -37,8 +42,28 @@ export async function initDatabase(uid, inTestMode = true) {
   schema = inTestMode ? SEEDS_RULES_TEST['schemas']['seeds'] : SEEDS_RULES['schemas']['seeds'];
   let newDb = await createDatabase('seeds_app', uid);
   let collectionName = inTestMode ? 'seedsTest' : 'seeds';
-  await addCollection(newDb, collectionName, schema);
-  activeSeedsCollection = newDb[collectionName];
+  activeSeedsCollection = await addCollection(newDb, collectionName, schema);
   setDatabase(newDb);
-  startFirestoreSync(newDb, uid, inTestMode, collectionName);
+  startFirestoreSync(newDb, uid, inTestMode, activeSeedsCollection);
+}
+
+/**
+ * Removes the database and sets relevant fields to null. Primarily used at the end of user interaction (e.g. user logs out)
+ * 
+ * @example
+ * if (hasDatabase()) {
+ *  removeDatabase()
+ * }
+ */
+export async function removeDatabase() {
+  if (!db) return;
+
+  activeSeedsCollection = null;
+  activeSeedsSubscription?.unsubscribe();
+  
+  try {
+    await db.remove();
+  } finally {
+    db = null;
+  }
 }
