7240d74cf50878e8a9df18862c9cd2e0f491e91f
auby <joaquin.tirona@student.ateneo.edu>
2026-07-06 12:35:55 +0800

    Added a class tag to filter field headers and cleaned up unused code

--- app_seeds/js/index.js
+++ app_seeds/js/index.js
@@ -107,7 +107,7 @@ function setUpFilterModal() {
 	const filters = getFilterFields(window.partners);
 	const filterSection = filterModal.getElementById('filter-section');
 	Object.keys(filters).forEach((field) => {
-		const filterHeader = `<h3>${field}</h3>`
+		const filterHeader = `<h3 class="filter-header">${field}</h3>`
 		filterSection.innerHTML += filterHeader;
 
 		filters[field].forEach((filter) => {
